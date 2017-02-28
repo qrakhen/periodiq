ba218a0519e7512ec2f6f04294a51ab6ca5b888e
Mennoxx <mennoxx@sygade.eu>
2017-02-28 23:30:53 +0100

    config.js WIP added, 2 default configs added, doc changes

--- static/config.js
+++ static/config.js
@@ -1,2 +1,86 @@
-/**
- * @todo Maus */
+ const Path  = require('path');
+ const fs    = require('fs');
+ const List  = require('sygtools').List;
+ const Debug = require('../debug.js');
+
+ /**
+  * Module for storing and loding configurations in the JSON-format.
+  * Usage:
+  *
+  * @class Config */
+ var Config = function() {
+
+     this.configs = new List();
+
+     /**
+      * Load and extend configurations.
+      * Usage:
+      *
+      * @function load
+      * @memberof Config
+      * @instance
+      * @param {FilePath} filePath   */
+     this.load = function(filePath) {
+         var split = filePath.split('.');
+         var config = split[split.length - 2];
+         this.configs[config] = JSON.parse(fs.readFileSync(JSON.stringify('config/pq.' + config + '.js')));
+
+         this.configs[config].storeTo = function(filePath) {
+             fs.writeFileSync(filePath, JSON.stringify('./config/pq.' + config + '.json'), function(err) {
+                 if(err) {
+                     return Debug.log(err);
+                 }
+                 Debug.log('SUCCESS: Config-file saved as ' + filePath);
+             });
+         };
+         this.configs[config].extendFrom = function(filePath) {
+             Debug.log(JSON.parse(filePath));
+             //foreEach(item in userConfigs) {
+             //  this.set(item.key, item.value)
+             //}
+         };
+     };
+
+     /**
+      * Get objects and values from configuration.
+      * Usage:
+      *
+      * @function get
+      * @memberof Config
+      * @instance
+      * @param {Config} config   */
+     this.get = function(config) {
+         if (this.configs[config] === undefined) {
+             try {
+                 this.load('.../pq.' + config + '.json');
+             } catch(err) {
+                 Debug.log(err);
+             }
+         } else {
+             return this.configs[config];
+         }
+     };
+
+     /**
+      * Sets objects and values from configuration.
+      * Usage:
+      *
+      * @function set
+      * @memberof Config
+      * @instance
+      * @param {string} key
+      * @param {string} value */
+     this.set = function(key, value) {
+
+     };
+
+     /**
+      * Lists the name of all default-configurations */
+     this.DEFAULT_CONFIG = {
+         window: './config/pq.window.js',
+         app:    './config/pq.app.json',
+         debug:  './config/pq.debug.json'
+     };
+ };
+
+ module.exports = new Config();
---
+++ static/config/pq.app.js
@@ -0,0 +1,16 @@
+/**
+ * Default configurations for periodiq apps
+ * This is used as source for creating a JSON-configuration-file for the specifoc app */
+module.exports = {
+
+    /* String - default theme of the app */
+    defaultTheme: 'default',
+
+    /* Integer - LogLevel determins how much of of the apps debug log-messesges are shown in console */
+    debugLogLevel: 0,
+
+    /* Object - GlobalShortcuts can be used anywhere in the app */
+    globalShortcuts: {
+
+    }
+}
